c140af6581f8e170e14d2f1d6738df24f914e9c7
tiantian <1500846601@qq.com>
2019-07-14 20:15:27 +0800

    初始化项目

---
+++ utils/util.js
@@ -0,0 +1,19 @@
+const formatNumber = (n) => {
+  const newN = n.toString();
+  return newN[1] ? newN : `0${newN}`;
+};
+
+const formatTime = (date) => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+
+  return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`;
+};
+
+module.exports = {
+  formatTime,
+};
